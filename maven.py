c238883b8d60678e3b41b35d6ca6b23b69e6c9b3
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2022-02-07 23:07:56 +0200

    Update maven.py

--- maven.py
+++ maven.py
@@ -9,9 +9,6 @@ st.write('''Maven is a tech company mostly focusing in solving socio-economic ch
 of technology and innovating in technology''')
 st.write('''Currently at Maven we are only developing software apps under Software Mavens but as time
 goes on we are aiming on doing more and more in technology''')
-st.subheader('Softwares')
-st.write('Our latest app is Pantonmath')
-st.write('pantonmath is an android question and answers application.\nOur aim in this application is to assist learners in answering comprehension questions.')
 st.subheader('Contact us')
 st.write('email address: mavenm113@gmail.com')
 st.write('mobile no: +27 79 395 5045')
